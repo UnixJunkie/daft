28a32e700b1e0e456e32d141326640c0216c753d
Francois BERENGER <francois.berenger@inria.fr>
2014-11-04 11:01:54 +0100

    added some comments

--- src/meta_data_node.ml
+++ src/meta_data_node.ml
@@ -1,5 +1,13 @@
 open Batteries
 open Types
 
+(* operations to support:
+   - get_chunk which_file which_chunk from_where
+   - send_chunk which_file which_chunk to_where
+   - list_chunks which_file
+   - list_files
+   - start
+   - stop *)
+
 let main () =
   failwith "not implemented yet"
