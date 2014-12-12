98a7d65a2df45857b60d67ea63a56bf65cda5a28
Francois BERENGER <francois.berenger@inria.fr>
2014-12-12 13:59:24 +0100

    indentation only

--- src/meta_data_server.ml
+++ src/meta_data_server.ml
@@ -1,5 +1,23 @@
 open Batteries
-open Types
+open Printf
 
 let main () =
+  (* setup logger *)
+  Log.set_log_level Log.DEBUG;
+  Log.set_output Legacy.stdout;
+  Log.color_on ();
+  (* setup MDS *)
+  let port = ref 0 in
+  let host = Utils.hostname () in
+  Arg.parse
+    [ "-p", Arg.Set_int port, "port where to listen" ]
+    (fun arg -> raise (Arg.Bad ("Bad argument: " ^ arg)))
+    (sprintf "usage: %s <options>" Sys.argv.(0))
+  ;
+  Log.info "MDS: %s:%d" host !port;
+  (* start all DSs *)
   failwith "not implemented yet"
+  (* wait for commands *)
+;;
+
+main ()
