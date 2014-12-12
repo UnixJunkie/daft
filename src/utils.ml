b01e91fc41ebbe3d7ea0e016d866b02979aa1348
Francois BERENGER <francois.berenger@inria.fr>
2014-12-12 13:32:16 +0100

    added hostname and run_and_read

---
+++ src/utils.ml
@@ -0,0 +1,32 @@
+
+(* like `cmd` in shell
+   FBR: use the one in batteries upon next release *)
+let run_and_read cmd =
+  let string_of_file fn =
+    let buff_size = 1024 in
+    let buff = Buffer.create buff_size in
+    let ic = open_in fn in
+    let line_buff = String.create buff_size in
+    begin
+      let was_read = ref (input ic line_buff 0 buff_size) in
+      while !was_read <> 0 do
+        Buffer.add_substring buff line_buff 0 !was_read;
+        was_read := input ic line_buff 0 buff_size;
+      done;
+      close_in ic;
+    end;
+    Buffer.contents buff
+  in
+  let tmp_fn = Filename.temp_file "" "" in
+  let cmd_to_run = cmd ^ " > " ^ tmp_fn in
+  let status = Unix.system cmd_to_run in
+  let output = string_of_file tmp_fn in
+  Unix.unlink tmp_fn;
+  (status, output)
+
+open Batteries
+
+let hostname (): string =
+  let stat, res = run_and_read "hostname -f" in
+  assert(stat = Unix.WEXITED 0);
+  String.strip res (* rm trailing '\n' *)
