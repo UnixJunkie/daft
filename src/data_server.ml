537757037788e8e4a4713a768f58accdbd45c43b
Francois BERENGER <francois.berenger@inria.fr>
2015-01-12 12:09:52 +0100

    put several attributes at global module scope

--- src/data_server.ml
+++ src/data_server.ml
@@ -7,7 +7,9 @@ module Log = Log.Make(struct let section = "DS" end) (* prefix logs *)
 
 (* module Client = Rpc_simple_client *)
 
-(* create local data store with unix UGO rights 700 *)
+(* create local data store with unix UGO rights 700
+   we take into account the port so that several DSs can be started on one
+   host at the same time, for tests *)
 let create_data_store (host: string) (port: int): string =
   let tmp_dir = Fn.get_temp_dir_name () in
   let data_store_root = sprintf "%s/%s:%d.ds" tmp_dir host port in
@@ -19,41 +21,42 @@ let create_data_store (host: string) (port: int): string =
 let delete_data_store (ds: string): int =
   Sys.command ("rm -rf " ^ ds)
 
+(* setup data server *)
+let ds_log_fn = ref ""
+let ds_host = Utils.hostname ()
+let ds_port = ref 0
+let mds_host = ref ""
+let mds_port = ref (-1)
+
 let main () =
   (* setup logger *)
   Logger.set_log_level Logger.DEBUG;
   Logger.set_output Legacy.stdout;
   Logger.color_on ();
-  (* setup data server *)
-  let port = ref 0 in
-  let host = Utils.hostname () in
-  let server_name = ref "" in
-  let server_port = ref (-1) in
-  let log_fn = ref "" in
   (* options parsing *)
   Arg.parse
-    [ "-l", Arg.Set_string log_fn, "<filename> where to log";
-      "-p", Arg.Set_int port, "<port> where to listen";
-      "-s", Arg.Set_string server_name, "<server> hostname";
-      "-sp", Arg.Set_int server_port, "<port> server port" ]
+    [ "-l", Arg.Set_string ds_log_fn, "<filename> where to log";
+      "-p", Arg.Set_int ds_port, "<port> where to listen";
+      "-s", Arg.Set_string mds_host, "<server> hostname";
+      "-sp", Arg.Set_int mds_port, "<port> server port" ]
     (fun arg -> raise (Arg.Bad ("Bad argument: " ^ arg)))
     (sprintf "usage: %s <options>" Sys.argv.(0))
   ;
   (* check options *)
-  if !server_name = "" then begin
+  if !ds_log_fn <> "" then begin (* don't log anything before that *)
+    let log_out = Legacy.open_out !ds_log_fn in
+    Logger.set_output log_out
+  end;
+  if !mds_host = "" then begin
     Log.fatal "-s is mandatory";
     exit 1;
   end;
-  if !server_port = -1 then begin
+  if !mds_port = -1 then begin
     Log.fatal "-sp is mandatory";
     exit 1;
   end;
-  if !log_fn <> "" then begin
-    Log.fatal "-l unimplemented yet";
-    exit 1;
-  end;
-  Log.info "Will connect to %s:%d" !server_name !server_port;
-  let data_store = create_data_store host !port in
+  Log.info "Will connect to %s:%d" !mds_host !mds_port;
+  let data_store = create_data_store !mds_host !mds_port in
 (*
   let connector = Rpc_client.Inet (server_name, server_port) in
   let protocol = Rpc.Tcp in
