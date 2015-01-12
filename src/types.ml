3558ebc548c3762ece1a57fcaeba956cf2ede051
Francois BERENGER <francois.berenger@inria.fr>
2015-01-12 15:50:30 +0100

    added FileSet and started to use FileUtil

--- src/types.ml
+++ src/types.ml
@@ -1,6 +1,8 @@
 open Batteries
 open Printf
 
+module FU = FileUtil
+
 (* should be set on the MDS side and read-only for DSs *)
 let chunk_size = ref (1 * 1024 * 1024)
 
@@ -19,16 +21,29 @@ type chunk = { rank: int    ;
 module Chunk = struct
   type t = chunk
   let compare c1 c2 =
-    Int.compare c1.rank c2.rank
+    BatInt.compare c1.rank c2.rank
 end
 
-type file = { name  : string            ;
-              chunks: Set.Make(Chunk).t }
+type managed_file = { name: string  ;
+                      size: int64   ;
+                      stat: FU.stat }
+
+let create_managed_file name size stat =
+  { name; size; stat }
 
 module File = struct
-  type t = file
+  type t = managed_file
   let compare f1 f2 =
     String.compare f1.name f2.name
+end
+
+let dummy_stat = FU.stat "/dev/null"
+
+module FileSet = struct (* extend type with more operations *)
+  include Set.Make(File)
+  let contains_fn fn s =
+    let dummy = { name = fn ; size = Int64.zero ; stat = dummy_stat } in
+    mem dummy s
 end
 
 type files = Set.Make(File).t
