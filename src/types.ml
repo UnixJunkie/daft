3f31e70dca3f71c3f9b17df5ef0b1f0fae59fe31
Francois BERENGER <francois.berenger@inria.fr>
2015-01-15 16:06:34 +0100

    added rank in Node.t and nodes in Chunk.t

--- src/types.ml
+++ src/types.ml
@@ -4,47 +4,59 @@ open Printf
 module FU = FileUtil
 
 module Node = struct
-  type t = { host: string ; port: int }
-  let create host port =
-    { host ; port }
+  (* the rank allows to uniquely identify nodes; a la MPI *)
+  type t = { rank: int    ;
+             host: string ;
+             port: int    }
+  let create rank host port =
+    { rank; host; port }
+  let dummy () =
+    { rank = -1; host = "localhost"; port = -1 }
   let to_string n =
-    sprintf "%s:%d" n.host n.port
+    sprintf "%d|%s:%d" n.rank n.host n.port
+  let compare n1 n2 =
+    BatInt.compare n1.rank n2.rank
+end
+
+module NodeSet = struct
+  include Set.Make(Node)
 end
 
 module File = struct
 
   module Chunk = struct
     let default_size = 1024 * 1024
-    type t = { rank: int ;
-               size: int64 option (* None if default_size; (Some x) else *)
-               (* FBR: it has a list/array of nodes too *) }
-    let create rank size =
-      { rank ; size }
+    type t = { rank:  int          ;
+               size:  int64 option ; (* None if default_size; (Some x) else *)
+               nodes: NodeSet.t    } (* which nodes have this chunk *)
+    let create rank size node =
+      let nodes = NodeSet.singleton node in
+      { rank; size; nodes }
     let compare c1 c2 =
       BatInt.compare c1.rank c2.rank
   end
 
   module ChunkSet = struct
     include Set.Make(Chunk)
-    let create nb_chunks last_chunk_size =
+    let create nb_chunks last_chunk_size node =
       assert(nb_chunks > 0); (* DAFT is not for files of null size *)
       let rec loop acc i =
         if i = nb_chunks - 1 then
-          add (Chunk.create i last_chunk_size) acc
+          add (Chunk.create i last_chunk_size node) acc
         else
-          let new_acc = add (Chunk.create i None) acc in
+          let new_acc = add (Chunk.create i None node) acc in
           loop new_acc (i + 1)
       in
       loop empty 0
   end
 
-  type t = { name: string ;
-             size: int64 ;
-             stat: FU.stat ;
-             nb_chunks: int ;
-             chunks: ChunkSet.t }
-  let create name size stat nb_chunks last_chunk_size =
-    let chunks = ChunkSet.create nb_chunks last_chunk_size in
+  type t = { name:      string     ;
+             size:      int64      ;
+             stat:      FU.stat    ;
+             nb_chunks: int        ;
+             chunks:    ChunkSet.t }
+  let create name size stat nb_chunks last_chunk_size node =
+    let chunks = ChunkSet.create nb_chunks last_chunk_size node in
     { name; size; stat; nb_chunks ; chunks }
   let compare f1 f2 =
     String.compare f1.name f2.name
