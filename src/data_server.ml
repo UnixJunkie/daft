af35e04940595a58aef3b2557e904e84877107a2
Francois BERENGER <francois.berenger@inria.fr>
2015-01-15 15:36:15 +0100

    better CLI options and handling of them

--- src/data_server.ml
+++ src/data_server.ml
@@ -13,22 +13,29 @@ module FileSet = Types.FileSet
 
 (* ALL OPERATIONS ARE SYNCHRONOUS *)
 
+let uninitialized = -1
+
 (* setup data server *)
 let ds_log_fn = ref ""
 let ds_host = Utils.hostname ()
-let ds_port = ref 0
+let ds_port = ref uninitialized
+let ds_rank = ref uninitialized
 let mds_host = ref ""
-let mds_port = ref (-1)
-let chunk_size = ref (-1) (* must be set on startup and same for all DSs *)
+let mds_port = ref uninitialized
+let chunk_size = ref uninitialized (* DAFT global constant *)
 let local_state = ref FileSet.empty
 let data_store_root = ref ""
+let local_node = ref (Node.dummy ()) (* this node *)
 
 (* create local data store with unix UGO rights 700
    we take into account the port so that several DSs can be started on one
    host at the same time, for tests *)
-let create_data_store (host: string) (port: int): string =
+let create_data_store (): string =
+  assert(!local_node <> Node.dummy ());
   let tmp_dir = Fn.get_temp_dir_name () in
-  let data_store_root = sprintf "%s/%s:%d.ds" tmp_dir host port in
+  let data_store_root =
+    sprintf "%s/%s.ds" tmp_dir (Node.to_string !local_node)
+  in
   Unix.mkdir data_store_root 0o700;
   Log.info "I store in %s" data_store_root;
   data_store_root
@@ -81,7 +88,9 @@ let add_file (fn: string): T.answer =
       else begin (* update local state *)
         let nb_chunks, last_chunk_size = compute_chunks size in
         (* n.b. we keep the stat struct from the original file *)
-        let new_file = File.create fn size stat nb_chunks last_chunk_size in
+        let new_file =
+          File.create fn size stat nb_chunks last_chunk_size !local_node
+        in
         local_state := FileSet.add new_file !local_state;
         T.Ok
       end
@@ -96,8 +105,9 @@ let main () =
     [ "-cs", Arg.Set_int chunk_size, "<size> file chunk size";
       "-l", Arg.Set_string ds_log_fn, "<filename> where to log";
       "-p", Arg.Set_int ds_port, "<port> where to listen";
-      "-s", Arg.Set_string mds_host, "<server> hostname";
-      "-sp", Arg.Set_int mds_port, "<port> server port" ]
+      "-r", Arg.Set_int ds_rank, "<rank> rank among other data nodes";
+      "-mds", Arg.Set_string mds_host, "<server> MDS host";
+      "-mdsp", Arg.Set_int mds_port, "<port> MDS port" ]
     (fun arg -> raise (Arg.Bad ("Bad argument: " ^ arg)))
     (sprintf "usage: %s <options>" Sys.argv.(0))
   ;
@@ -106,20 +116,14 @@ let main () =
     let log_out = Legacy.open_out !ds_log_fn in
     Logger.set_output log_out
   end;
-  if !mds_host = "" then begin
-    Log.fatal "-s is mandatory";
-    exit 1;
-  end;
-  if !chunk_size = -1 then begin
-    Log.fatal "-cs is mandatory";
-    exit 1;
-  end;
-  if !mds_port = -1 then begin
-    Log.fatal "-sp is mandatory";
-    exit 1;
-  end;
+  if !chunk_size = uninitialized then (Log.fatal "-cs is mandatory"; exit 1);
+  if !mds_host = "" then (Log.fatal "-s is mandatory"; exit 1);
+  if !mds_port = uninitialized then (Log.fatal "-sp is mandatory"; exit 1);
+  if !ds_rank = uninitialized then (Log.fatal "-r is mandatory"; exit 1);
+  if !ds_port = uninitialized then (Log.fatal "-p is mandatory"; exit 1);
+  local_node := Node.create !ds_rank ds_host !ds_port;
   Log.info "Will connect to %s:%d" !mds_host !mds_port;
-  data_store_root := create_data_store !mds_host !mds_port;
+  data_store_root := create_data_store ();
   delete_data_store !data_store_root
 ;;
 
