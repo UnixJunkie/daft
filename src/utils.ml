698d5ec3cf8a8f1063d4a23dfbdf719ea8835a81
Francois BERENGER <francois.berenger@inria.fr>
2015-03-16 19:04:47 +0100

    added ANSI term colors added zmq_dummy_client_setup

--- src/utils.ml
+++ src/utils.ml
@@ -6,6 +6,18 @@ let default_ds_port = 8081
 let default_mds_port = 8082
 let default_chunk_size = 1_000_000 (* bytes *)
 
+(* ANSI terminal colors for UNIX: *)
+let fg_black = "\027[30m"
+let fg_red = "\027[31m"
+let fg_green = "\027[32m"
+let fg_yellow = "\027[33m"
+let fg_blue = "\027[34m"
+let fg_magenta = "\027[35m"
+let fg_cyan = "\027[36m"
+let fg_white = "\027[37m"
+let fg_default = "\027[39m"
+let fg_reset = "\027[0m"
+
 let sleep_ms ms =
   let (_, _, _) = Unix.select [] [] [] (float_of_int ms /. 1000.) in
   ()
@@ -65,6 +77,11 @@ let zmq_client_setup (host: string) (port: int) =
   let socket = ZMQ.Socket.create context ZMQ.Socket.req in
   let host_and_port = sprintf "tcp://%s:%d" host port in
   let () = ZMQ.Socket.connect socket host_and_port in
+  (context, socket)
+
+let zmq_dummy_client_setup () =
+  let context = ZMQ.Context.create () in
+  let socket = ZMQ.Socket.create context ZMQ.Socket.req in
   (context, socket)
 
 let zmq_cleanup socket context =
