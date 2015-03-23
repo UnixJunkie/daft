8073edb8543eb797cd540b6aef877a9d939ca011
Francois BERENGER <francois.berenger@inria.fr>
2015-03-23 11:09:54 +0100

    updated command names

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 # to run local tests
 
-pkill dft_mds # kill any MDS
+pkill daft_mds # kill any MDS
 
-pkill dft_ds # kill any DS
+pkill daft_ds # kill any DS
 
-./dft_mds -m machines & # one local MDS in background
+./daft_mds -m machines & # one local MDS in background
 
 rm -rf /tmp/*.ds # clean previous datastore
 
-./dft_ds -r 0 -mds `hostname -f` # one local DS
+./daft_ds -r 0 -mds `hostname -f` # one local DS
