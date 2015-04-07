9ad8e317e489209181d2b8fb2b1f1968e870d814
Francois BERENGER <francois.berenger@inria.fr>
2015-04-07 14:58:40 +0200

    start several local DSs

--- test.sh
+++ test.sh
@@ -8,7 +8,9 @@ rm -rf /tmp/*.ds # clean previous datastore
 
 ./daft_mds -m machines & # one MDS
 
-./daft_ds -r 0 -mds `hostname -f` & # one DS
+./daft_ds -r 0 -mds `hostname -f` -p 8083 & # one DS
+./daft_ds -r 1 -mds `hostname -f` -p 8084 & # one DS
+./daft_ds -r 2 -mds `hostname -f` -p 8085 & # one DS
 
 sleep 1s
 
