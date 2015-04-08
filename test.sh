8149727ebb63550ad4c6727ca16009eaff8ae67f
Francois BERENGER <francois.berenger@inria.fr>
2015-04-08 17:52:20 +0200

    created reset function

--- test.sh
+++ test.sh
@@ -1,17 +1,22 @@
 # to run local tests
 
-pkill daft_mds # kill any MDS
+function reset () {
+    pkill daft_mds    # kill MDS
+    pkill daft_ds     # kill all DSs
+    \rm -rf /tmp/*.ds # clean previous local datastore
+}
 
-pkill daft_ds # kill any DS
-
-rm -rf /tmp/*.ds # clean previous datastore
+reset
 
 ./daft_mds -m machines & # one MDS
 
+# 3 local DSs
 ./daft_ds -m machines -r 0 -mds `hostname -f` -p 8083 & # one DS
 ./daft_ds -m machines -r 1 -mds `hostname -f` -p 8084 & # one DS
 ./daft_ds -m machines -r 2 -mds `hostname -f` -p 8085 & # one DS
 
+# automatic tests
 sleep 1s
-
 echo quit | ./daft_cli -ds meleze.ens.fr:8083
+## interactive tests
+# ./daft_cli -ds meleze.ens.fr:8083
