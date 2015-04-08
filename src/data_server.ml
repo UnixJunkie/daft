4862e0d51d10d2caec82c372f3271e5c20824046
Francois BERENGER <francois.berenger@inria.fr>
2015-04-08 12:21:21 +0200

    can forward the Add_file_ack/nack from the MDS to the local CLI

--- src/data_server.ml
+++ src/data_server.ml
@@ -24,6 +24,7 @@ let uninitialized = -1
 let ds_log_fn = ref ""
 let ds_host = Utils.hostname ()
 let ds_port_in = ref Utils.default_ds_port_in
+let cli_port_in = ref Utils.default_cli_port_in
 let ds_rank = ref uninitialized
 let machine_file = ref ""
 let mds_host = ref "localhost"
@@ -72,8 +73,10 @@ let compute_chunks (size: int64) =
   (nb_chunks, last_chunk_size_opt)
 
 let add_file (fn: string): ds_to_cli =
-  if FileSet.contains_fn fn !local_state then Already_here
-  else if Sys.is_directory fn then Is_directory
+  if FileSet.contains_fn fn !local_state then
+    Add_file_cmd_nack (fn, Already_here)
+  else if Sys.is_directory fn then
+    Add_file_cmd_nack (fn, Is_directory)
   else
     FU.( (* opened FU to get rid of warning 40 *)
       let stat = FU.stat fn in
@@ -90,7 +93,8 @@ let add_file (fn: string): ds_to_cli =
       FU.cp ~follow:FU.Follow ~force:FU.Force ~recurse:false [fn] dest_fn;
       (* check cp succeeded based on new file's size *)
       let stat' = FU.stat dest_fn in
-      if stat'.size <> stat.size then Copy_failed
+      if stat'.size <> stat.size then
+        Add_file_cmd_nack (fn, Copy_failed)
       else begin (* update local state *)
         let nb_chunks, last_chunk_size = compute_chunks size in
         (* n.b. we keep the stat struct from the original file *)
@@ -98,7 +102,7 @@ let add_file (fn: string): ds_to_cli =
           File.create fn size stat nb_chunks last_chunk_size !local_node
         in
         local_state := FileSet.add new_file !local_state;
-        Ok
+        Add_file_cmd_ack fn
       end
     )
 
@@ -111,6 +115,7 @@ let main () =
   Arg.parse
     [ "-cs", Arg.Set_int chunk_size, "<size> file chunk size";
       "-l", Arg.Set_string ds_log_fn, "<filename> where to log";
+      "-cli", Arg.Set_int cli_port_in, "<port> where the CLI is listening";
       "-p", Arg.Set_int ds_port_in, "<port> where to listen";
       "-r", Arg.Set_int ds_rank, "<rank> rank among other data nodes";
       "-m", Arg.Set_string machine_file,
@@ -130,6 +135,7 @@ let main () =
   if !mds_port_in = uninitialized then (Log.fatal "-sp is mandatory"; exit 1);
   if !ds_rank = uninitialized then (Log.fatal "-r is mandatory"; exit 1);
   if !ds_port_in = uninitialized then (Log.fatal "-p is mandatory"; exit 1);
+  if !cli_port_in = uninitialized then (Log.fatal "-cli is mandatory"; exit 1);
   if !machine_file = "" then abort "-m is mandatory";
   let int2node = Utils.data_nodes_array !machine_file in
   Log.info "read %d host(s)" (A.length int2node);
@@ -140,6 +146,8 @@ let main () =
   Log.info "binding server to %s:%d" "*" !ds_port_in;
   let ctx = ZMQ.Context.create () in
   let incoming = Utils.(zmq_socket Pull ctx "*" !ds_port_in) in
+  (* feedback socket to the local CLI *)
+  let to_cli = Utils.(zmq_socket Push ctx "*" !cli_port_in) in
   (* register at the MDS *)
   Log.info "connecting to MDS %s:%d" !mds_host !mds_port_in;
   let to_mds = Utils.(zmq_socket Push ctx !mds_host !mds_port_in) in
@@ -152,15 +160,23 @@ let main () =
       let encoded = Sock.recv incoming in
       let message = For_DS.decode encoded in
       begin match message with
-        | For_DS.From_MDS (Send_to_req (_ds_rank, _fn, _chunk)) ->
+        | For_DS.From_MDS (Send_to_req (_ds_rank, _fn, _chunk)) -> (* ------ *)
           abort "Send_to_req"
-        | For_DS.From_MDS Quit_cmd ->
+        | For_DS.From_MDS Quit_cmd -> (* ----------------------------------- *)
           let _ = Log.info "got Quit" in
           not_finished := false
-        | For_DS.From_DS (Chunk (_fn, _chunk, _data)) ->
+        | For_DS.From_MDS (Add_file_ack fn) -> (* -------------------------- *)
+          let ack = For_CLI.encode (For_CLI.From_DS (Add_file_cmd_ack fn)) in
+          Sock.send to_cli ack
+        | For_DS.From_MDS (Add_file_nack fn) -> (* ------------------------- *)
+          let nack =
+            For_CLI.encode (For_CLI.From_DS (Add_file_cmd_nack (fn, Already_here)))
+          in
+          Sock.send to_cli nack
+        | For_DS.From_DS (Chunk (_fn, _chunk, _data)) -> (* ---------------- *)
           abort "Chunk"
-        | For_DS.From_CLI Add_file _f ->
-          abort "Add_file"
+        | For_DS.From_CLI Add_file_cmd_req _f -> (* ------------------------ *)
+          abort "Add_file_cmd_req"
       end
     done;
   with exn -> begin
