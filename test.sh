135654cd377c3d8fb98270fc844f105f9868d1d1
Francois BERENGER <francois.berenger@inria.fr>
2015-04-09 10:34:29 +0200

    - added -cli option to all servers - added more commands to launch tests by hand

--- test.sh
+++ test.sh
@@ -8,15 +8,26 @@ function reset () {
 
 reset
 
-./daft_mds -m machines & # one MDS
+./daft_mds -m machines -cli meleze.ens.fr:8000 & # one MDS
 
 # 3 local DSs
-./daft_ds -m machines -r 0 -mds `hostname -f` -p 8083 & # one DS
-./daft_ds -m machines -r 1 -mds `hostname -f` -p 8084 & # one DS
-./daft_ds -m machines -r 2 -mds `hostname -f` -p 8085 & # one DS
+./daft_ds -m machines -r 0 -mds `hostname -f` -p 8083 -cli meleze.ens.fr:8000 & # one DS
+./daft_ds -m machines -r 1 -mds `hostname -f` -p 8084 -cli meleze.ens.fr:8000 & # one DS
+./daft_ds -m machines -r 2 -mds `hostname -f` -p 8085 -cli meleze.ens.fr:8000 & # one DS
 
 # automatic tests
 sleep 1s
 echo quit | ./daft_cli -ds meleze.ens.fr:8083
 ## interactive tests
+# ./daft_cli -ds meleze.ens.fr:8083
+
+# # to run all processes separately by hand
+# FBR: find a way to fire each one in a different xterm so that logs
+#      are not interlaced
+# ./daft_mds -m machines -cli meleze.ens.fr:8000
+
+# ./daft_ds -m machines -r 0 -mds `hostname -f` -p 8083 -cli meleze.ens.fr:8000
+# ./daft_ds -m machines -r 1 -mds `hostname -f` -p 8084 -cli meleze.ens.fr:8000
+# ./daft_ds -m machines -r 2 -mds `hostname -f` -p 8085 -cli meleze.ens.fr:8000
+
 # ./daft_cli -ds meleze.ens.fr:8083
