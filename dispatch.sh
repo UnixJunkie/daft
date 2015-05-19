d1718bebd64eae0dce1d5d20fc4b6438bde6edfa
Francois BERENGER <francois.berenger@inria.fr>
2015-05-19 18:44:57 +0200

    ./dispatch.sh [hostname] send all DAFT's executables to a remote host via scp

---
+++ dispatch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#set -x
+
+# copy all DAFT exes to a remote host
+
+if [ "$#" -ne 1 ] ; then
+    echo "usage: "$0" [hostname]"
+fi
+
+scp \
+dist/build/daft/daft \
+dist/build/daft_mds/daft_mds \
+dist/build/daft_ds/daft_ds $1:
