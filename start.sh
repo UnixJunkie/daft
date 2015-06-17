56d49f21fcc8f45ef5d18ac7f49cb098796f9cda
Camille Coti <coti@maximum.lipn.univ-paris13.fr>
2015-06-17 12:13:38 +0200

    Start the DAFT infrastructure

---
+++ start.sh
@@ -0,0 +1,24 @@
+# to run local tests
+
+set -x
+
+function reset () {
+    pkill daft_mds    # kill MDS
+    pkill daft_ds     # kill all DSs
+    \rm -rf /tmp/*.ds # clean previous local datastore
+}
+
+reset
+
+OPTS=""
+# OPTS="-z" # to test on the fly compression of all communications
+
+./daft_mds $OPTS -m machines -cli `hostname -f`:8000 & # one MDS
+
+./daft_ds $OPTS -m machines -r 0 -mds `hostname -f` -p 8083 -cli `hostname -f`:8000 & # one DS
+./daft_ds $OPTS -m machines -r 1 -mds `hostname -f` -p 8084 -cli `hostname -f`:8000 & # one DS
+./daft_ds $OPTS -m machines -r 2 -mds `hostname -f` -p 8085 -cli `hostname -f`:8000 & # one DS
+
+# export env. vars. so that the cli invocation is simpler
+export DAFT_MDS=`hostname -f`:8082
+export DAFT_DS=`hostname -f`:8083
