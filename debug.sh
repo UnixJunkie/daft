41e24c155583c52db22b04fd6728cee40022bbaf
Francois BERENGER <francois.berenger@inria.fr>
2015-06-17 20:24:24 +0200

    added debug.sh

---
+++ debug.sh
@@ -0,0 +1,21 @@
+# to run local tests
+
+set -x
+
+function reset () {
+    pkill daft_mds    # kill MDS
+    pkill daft_ds     # kill all DSs
+    \rm -rf /tmp/*.ds # clean previous local datastore
+}
+
+reset
+
+./daft_mds.d -m machines -cli `hostname -f`:8000 & # one MDS
+
+./daft_ds.d -m machines -r 0 -mds `hostname -f` -p 8083 -cli `hostname -f`:8000 & # one DS
+./daft_ds.d -m machines -r 1 -mds `hostname -f` -p 8084 -cli `hostname -f`:8000 & # one DS
+./daft_ds.d -m machines -r 2 -mds `hostname -f` -p 8085 -cli `hostname -f`:8000 & # one DS
+
+# export env. vars. so that the cli invocation is simpler
+export DAFT_MDS=`hostname -f`:8082
+export DAFT_DS=`hostname -f`:8083
