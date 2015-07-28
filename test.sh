7cbaeea46ec3d13fa2247c880e5efcd3cc19211c
Francois BERENGER <francois.berenger@inria.fr>
2015-07-28 10:50:23 +0200

    DS doesn't have -cli option anymore

--- test.sh
+++ test.sh
@@ -14,9 +14,9 @@ OPTS=""
 
 ./daft_mds $OPTS -m machines -cli `hostname -f`:8000 & # one MDS
 
-./daft_ds $OPTS -m machines -r 0 -mds `hostname -f` -p 8083 -cli `hostname -f`:8000 & # one DS
-./daft_ds $OPTS -m machines -r 1 -mds `hostname -f` -p 8084 -cli `hostname -f`:8000 & # one DS
-./daft_ds $OPTS -m machines -r 2 -mds `hostname -f` -p 8085 -cli `hostname -f`:8000 & # one DS
+./daft_ds $OPTS -m machines -r 0 -mds `hostname -f` -p 8083 & # one DS
+./daft_ds $OPTS -m machines -r 1 -mds `hostname -f` -p 8084 & # one DS
+./daft_ds $OPTS -m machines -r 2 -mds `hostname -f` -p 8085 & # one DS
 
 # export env. vars. so that the cli invocation is simpler
 export DAFT_MDS=`hostname -f`:8082
