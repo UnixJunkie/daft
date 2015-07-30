75f0579287208fa74a0f51491fd1de6dae4408cc
Francois BERENGER <francois.berenger@inria.fr>
2015-07-30 14:10:14 +0200

    don't run anything in case of #args problem

--- bin/dispatch.sh
+++ bin/dispatch.sh
@@ -6,12 +6,12 @@
 
 if [ "$#" -ne 1 ] ; then
     echo "usage: "$0" [hostname]"
+else
+    scp \
+        machines \
+        bootstrap.sh \
+        dist/build/daft/daft \
+        dist/build/daft_mds/daft_mds \
+        dist/build/daft_ds/daft_ds \
+        $1:
 fi
-
-scp \
-machines \
-bootstrap.sh \
-dist/build/daft/daft \
-dist/build/daft_mds/daft_mds \
-dist/build/daft_ds/daft_ds \
-$1:
