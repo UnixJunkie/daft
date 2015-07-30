c6e4907e400c25b8bea5cbb4411ef64cac84770f
Francois BERENGER <francois.berenger@inria.fr>
2015-07-30 14:13:29 +0200

    rm OPTS

--- test.sh
+++ test.sh
@@ -10,19 +10,17 @@ function reset () {
 
 reset
 
-OPTS=""
+./daft_mds -m machines & # one MDS
 
-./daft_mds $OPTS -m machines & # one MDS
-
-./daft_ds $OPTS -m machines -r 0 -mds `hostname -f` -p 8083 & # one DS
-./daft_ds $OPTS -m machines -r 1 -mds `hostname -f` -p 8084 & # one DS
-./daft_ds $OPTS -m machines -r 2 -mds `hostname -f` -p 8085 & # one DS
+./daft_ds -m machines -r 0 -mds `hostname -f` -p 8083 & # one DS
+./daft_ds -m machines -r 1 -mds `hostname -f` -p 8084 & # one DS
+./daft_ds -m machines -r 2 -mds `hostname -f` -p 8085 & # one DS
 
 # export env. vars. so that the cli invocation is simpler
 export DAFT_MDS=`hostname -f`:8082
 export DAFT_DS=`hostname -f`:8083
 
 sleep 1s
-./daft -r 0 $OPTS -p 8000 -c quit
+./daft -r 0 -p 8000 -c quit
 
 ps
