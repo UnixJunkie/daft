d9cbb57c279a4749823eb93a8ec2fe49e8560c2e
Francois BERENGER <francois.berenger@inria.fr>
2015-07-30 14:12:40 +0200

    rm OPTS

--- start.sh
+++ start.sh
@@ -10,14 +10,11 @@ function reset () {
 
 reset
 
-OPTS=""
-# OPTS="-z" # to test on the fly compression of all communications
+./daft_mds -m machines -cli `hostname -f`:8000 & # one MDS
 
-./daft_mds $OPTS -m machines -cli `hostname -f`:8000 & # one MDS
-
-./daft_ds $OPTS -m machines -r 0 -mds `hostname -f` -p 8083 -cli `hostname -f`:8000 & # one DS
-./daft_ds $OPTS -m machines -r 1 -mds `hostname -f` -p 8084 -cli `hostname -f`:8000 & # one DS
-./daft_ds $OPTS -m machines -r 2 -mds `hostname -f` -p 8085 -cli `hostname -f`:8000 & # one DS
+./daft_ds -m machines -r 0 -mds `hostname -f` -p 8083 -cli `hostname -f`:8000 & # one DS
+./daft_ds -m machines -r 1 -mds `hostname -f` -p 8084 -cli `hostname -f`:8000 & # one DS
+./daft_ds -m machines -r 2 -mds `hostname -f` -p 8085 -cli `hostname -f`:8000 & # one DS
 
 # export env. vars. so that the cli invocation is simpler
 export DAFT_MDS=`hostname -f`:8082
