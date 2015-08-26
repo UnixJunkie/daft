518366f5267be941308152b6ecca18e95588ab6b
Francois BERENGER <francois.berenger@inria.fr>
2015-08-26 12:10:59 +0200

    rm leftover counter file

--- test.sh
+++ test.sh
@@ -7,6 +7,7 @@ function reset () {
     pkill daft_ds     # kill all DSs
     pkill daft        # kill CLI
     \rm -rf /tmp/*.ds # clean previous local datastore
+    \rm -f CLI.msg_counter # force to start a new session
 }
 
 reset
