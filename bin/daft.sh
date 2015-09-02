9e16cc3ea44140d14f3daafd1760a82f6c82d9b1
Francois BERENGER <francois.berenger@inria.fr>
2015-09-02 10:38:13 +0200

    added shell wrappers for each command

---
+++ bin/daft.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#set -x
+
+# edit DAFT_ROOT to reflect your installation
+DAFT_ROOT=~/src/daft
+
+export LD_LIBRARY_PATH=~/usr/zmq-4.0.4/lib:~/usr/lz4-r129/lib
+
+$DAFT_ROOT/daft "$*"
