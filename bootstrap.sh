9c1a61ae62a8968de9b16265fdaaae3fff49dbd5
Francois BERENGER <francois.berenger@inria.fr>
2015-10-16 15:19:20 +0200

    no more LZ4

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,15 +13,5 @@ mkdir -p $HOME/usr/zmq-4.0.4
 (make 2>&1) > zmq_make.log
 make install
 
-# source-based installer for LZ4
-if [ ! -f r129.tar.gz ] ; then
-    wget https://github.com/Cyan4973/lz4/archive/r129.tar.gz
-fi
-tar xzf r129.tar.gz
-cd lz4-r129
-mkdir -p $HOME/usr/lz4-r129
-make all
-cp -a lib $HOME/usr/lz4-r129/
-
 # LD_LIBRARY_PATH needs to be updated
-echo \~/usr/zmq-4.0.4/lib:\~/usr/lz4-r129/lib
+echo \~/usr/zmq-4.0.4/lib
