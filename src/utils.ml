b6ea3eee2df6aac85eaf219c34e47151239bee11
Francois BERENGER <francois.berenger@inria.fr>
2016-01-08 09:58:47 +0100

    added unix_time_to_string

--- src/utils.ml
+++ src/utils.ml
@@ -396,3 +396,14 @@ let rec fold_while f p acc = function
 
 let id x =
   x
+
+let unix_time_to_string tm =
+  Unix.(
+    sprintf "%02d/%02d/%04d-%02d:%02d:%02d"
+      tm.tm_mday
+      tm.tm_mon
+      (tm.tm_year + 1900)
+      tm.tm_hour
+      tm.tm_min
+      tm.tm_sec
+  )
