d0da9ae34c8f225ad3fb75e43525a6814f0ce534
Francois BERENGER <francois.berenger@inria.fr>
2016-01-27 10:49:09 +0100

    the MDS and DSs are launched in separate xterms to not cluter the terminal with the CLI running

--- test.sh
+++ test.sh
@@ -13,23 +13,20 @@ echo $host:8084      >> machines
 echo $host:8085      >> machines
 chmod 644 machines # perms are too wide on purpose
 
-# # interactive use
-# ./daft_mds -m machines &
-# ./daft_ds  -m machines -p 8083 &
-# ./daft_ds  -m machines -p 8084 &
-# ./daft_ds  -m machines -p 8085 &
-# ./daft     -m machines -i
-
-./daft_mds -v -m machines &
+xterm -hold -e "./daft_mds -v -m machines" &
+MDS=$!
 
 sleep 1s # reading /dev/random takes time
 
 ls -l machines # perms should be strict now
 cat machines
 
-./daft_ds -v -m machines -p 8083 &
-./daft_ds -v -m machines -p 8084 &
-./daft_ds -v -m machines -p 8085 &
+xterm -hold -e "./daft_ds -v -m machines -p 8083" &
+DS0=$!
+xterm -hold -e "./daft_ds -v -m machines -p 8084" &
+DS1=$!
+xterm -hold -e "./daft_ds -v -m machines -p 8085" &
+DS2=$!
 
 sleep 1s
 
@@ -41,3 +38,6 @@ fi
 
 sleep 1s
 ps
+
+kill $MDS $DS0 $DS1 $DS2
+
