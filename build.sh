f342cf68f79c5ddf6fd14dd99ab41bf15f6db1b3
Francois Berenger <unixjunkie@sdf.org>
2023-01-11 14:08:24 +0900

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-# # with oasis
-# oasis setup
-# ocaml setup.ml -configure --prefix `opam config var prefix`
-# ocaml setup.ml -build
-# #ocaml setup.ml -install
-
-# with obuild
-obuild configure
-obuild build
+dune build @all
